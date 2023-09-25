20f71707147f25b92ccf908daabd1072814270e0
mauroluciano <mauroluciano911@gmail.com>
2023-09-25 15:54:19 +0200

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Mauro";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>primo PHP</title>
+</head>
+<body>
+    <?php echo "$name"?>
+</body>
+</html>
